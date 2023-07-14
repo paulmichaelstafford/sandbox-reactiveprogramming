be966a4ef3db73ecacb848765c69b9f5320db857
Paul Stafford <paul.stafford@visual-meta.com>
2023-07-14 11:26:49 +0200

    cleaning

--- src/main/kotlin/com/oghamstone/sandbox/sandboxreactiveprogramming/SandboxReactiveProgrammingApplication.kt
+++ src/main/kotlin/com/oghamstone/sandbox/sandboxreactiveprogramming/SandboxReactiveProgrammingApplication.kt
@@ -1,8 +1,6 @@
 package com.oghamstone.sandbox.sandboxreactiveprogramming
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
